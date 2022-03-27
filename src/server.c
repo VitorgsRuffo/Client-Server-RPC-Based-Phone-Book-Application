2fee4c6b2d215c31b6ef103ddcee030fe8a16671
wellinton <wellintonpiassa@hotmail.com>
2022-03-27 20:28:08 -0300

    Refactoring create server fuction

--- src/server.c
+++ src/server.c
@@ -3,13 +3,51 @@
 
 int * create_1_svc (record *argp, struct svc_req *rqstp){
     int* operationResult = (int*) malloc(sizeof(int));
-    
+    *operationResult = 0;
+
     FILE* database;
-    database = fopen("database.bin", "ab");
-    if(database != NULL)
-        if(fwrite(argp, sizeof(record), 1, database) == 1)
+    database = fopen("database.bin", "r+b");
+    if(database == NULL)
+        return operationResult;
+        
+    //calculating database size:
+    long databaseSizeInBytes;
+    fseek(database, 0, SEEK_END);
+    databaseSizeInBytes = ftell(database);
+    rewind(database);
+
+    long databaseSizeInRecords = databaseSizeInBytes / sizeof(record);
+
+    //allocating memory to contain whole database in memory:
+    //(for the cases where the database is greater than memory this obviously is not going to work)
+    record* buffer;
+    buffer = (record*) malloc(sizeof(record)*databaseSizeInRecords);
+    if (buffer == NULL) {
+        fclose(database);
+        return operationResult;
+    }
+
+    //copying the file (database) into the memory buffer:
+    if(fread(buffer, sizeof(record), databaseSizeInRecords, database) != databaseSizeInRecords)
+        return operationResult;
+
+    //iterating through buffer in order to find the requested record: 
+    //(This is not the most efficient way to do this. Implement the database with a hashfile would make searching faster.)
+    for(int i = 0; i<databaseSizeInRecords; i++){
+        if(!strcmp(buffer[i].name, "invalid")){
+            printf("Achou");
+            fseek(database, i*sizeof(record), SEEK_SET);
+            fwrite(argp, sizeof(buffer[i]), 1, database);
+            fclose(database);
             *operationResult = 1;
-    fclose(database);
+            return (operationResult);
+        }
+    }
+
+    if(fwrite(argp, sizeof(record), 1, database) == 1) {
+        fclose(database);
+        *operationResult = 1;
+    }
 
     return (operationResult);
 }
